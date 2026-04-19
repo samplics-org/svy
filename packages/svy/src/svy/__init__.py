f70168a43be63e8a7763060705548e357a815dfb
Mamadou S Diallo <msdiallo@samplics.org>
2026-04-18 22:22:24 -0400

    svy 0.17.1: fix bootstrap/BRR determinism and trimming redistribution

--- packages/svy/src/svy/__init__.py
+++ packages/svy/src/svy/__init__.py
@@ -124,7 +124,7 @@ from svy.weighting import Threshold, TrimConfig, TrimResult
 # Ensure no “No handlers could be found” warnings in user apps
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-__version__ = "0.17.0"
+__version__ = "0.17.1"
 
 __all__ = [
     # --- Modules ----
